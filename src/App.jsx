abf5b000cf115e8bcdf983da8d028dc3585de7d3
bradygrapentine <brady.grapentine@gmail.com>
2021-05-27 16:33:11 -0400

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export class App extends Component {
+  state = {
+    results: [],
+  }
+
+  // fetchData = async () => {
+  //   const response = await fetch('http://www.dnd5eapi.co/api/monsters')
+  //   const data = await response.json()
+  //   this.setState({ results: data.results })
+  //   console.log(data)
+  //   console.log(this.state.results)
+  // }
+
+  fetchData = async () => {
+    await axios.get('http://www.dnd5eapi.co/api/monsters').then(response => {
+      this.setState({ results: response.data.results })
+    })
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
+
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <>
+        <h1>Hello, Fetch</h1>
+        <div>
+          <ul>
+            {this.state.results.map(monster => {
+              return (
+                <li key={monster.index}>
+                  <a href={`http://www.dnd5eapi.co${monster.url}`}>
+                    {monster.name}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </>
+    )
   }
 }
